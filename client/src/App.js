363e391b8204f43ce90c4a61861779afea4aea2f
skdksldk <tnwns1485@naver.com>
2023-08-11 11:39:39 +0900

    Add Login Page

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,25 @@ import NotFound from './pages/NotFound';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ProSidebarProvider } from 'react-pro-sidebar';
-
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Log from './pages/Log';
 
 const App = () => {
   return (
     <>
       <ToastContainer />
+      <Provider store={store}>
         <ProSidebarProvider>
           <BrowserRouter>
             <Routes>
               <Route path='/' element={<Home />} />
+              <Route path='/login' element={<Log />} />
               <Route path='*' element={<NotFound />} />
             </Routes>
           </BrowserRouter>
         </ProSidebarProvider>
+        </Provider>
     </>
   )
 }
